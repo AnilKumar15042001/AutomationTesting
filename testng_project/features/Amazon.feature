2b17c70dd1f8f2b6c859f6502c77b4ea97144fa2
AnilKumar15042001 <aniluttarakabat12345@gmail.com>
2024-09-22 21:27:33 +0530

    Assignment completed

--- testng_project/features/Amazon.feature
+++ testng_project/features/Amazon.feature
@@ -23,15 +23,15 @@ Feature: Amazon AddToCart functionality
     And User close the browser
     
     Examples:
-    | ProductName | Quantity |
-    #| shoes | 2 |
-    #| mobile | 2 |
-    #| shirt | 3 |
-    #| pant | 2 |
-    #| mouse | 1 |
-    #| laptop | 1 |
-    #| t-shirt | 3 |
-    #| watch | 2 |
-    | bag | 2 |
-    | cap | 3 |
+    |ProductName|Quantity|
+    |shoes      |2       |
+    |mobile     |2       |
+    |shirt      |3       |
+    |pant       |2       |
+    |mouse      |1       |
+    |laptop     |1       |
+    |t-shirt    |3       |
+    |watch      |2       |
+    |bag        |2       |
+    |cap        |3       |
     
--- testng_project/src/test/java/stepdefinitions/Amazon.java
+++ testng_project/src/test/java/stepdefinitions/Amazon.java
@@ -26,21 +26,6 @@ import utils.WindowsUtils;
 
 public class Amazon {
 
-	@Given("User launch the browser and enter url")
-	public void user_launch_the_browser_and_enter_url() {
-		System.out.println("browser launched");
-	}
-
-	@When("User enter username and password then click on login button")
-	public void user_enter_username_and_password_then_click_on_login_button() {
-		System.out.println("login success");
-	}
-
-	@Then("User should see the Dashboard page")
-	public void user_should_see_the_dashboard_page() {
-		System.out.println("dashboard page is displayed");
-	}
-
 	@Given("User launch the browser")
 	public void user_launch_the_browser() throws Exception {
 		BrowserUtils.openBrowser();
